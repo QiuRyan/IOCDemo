c1293901999fb9414d9da12702ee6947a56bf9c9
邱彦军 <qiuyanjun2009@163.com>
2017-09-30 09:17:48 +0800

    first

---
+++ IOC.Demo.Lib/Services/UserService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IOC.Demo.Lib.Interface;
+
+namespace IOC.Demo.Lib
+{
+    public class UserService : IUser
+    {
+    }
+}
